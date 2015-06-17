b957eec32d5ff5895e9d7346af3d08da67b61749
Bonfire <bonfire.hf@gmail.com>
2015-06-17 15:20:44 -0400

    Fixed some syntax errors and followed some common conventions

--- 00 - Name Generator.py
+++ 00 - Name Generator.py
@@ -1,24 +1,25 @@
-import requests, json # Import every package we need.
+import requests
+# Import every package we need.
 
-print "Name Generator - Bonfire" # Name of the program, and it's creator.
-print "Credits to RandomUser.Me for the endless name list!" # http://randomuser.me (Lets hope that they stay up!)
+print "Name Generator - Bonfire"  # Name of the program, and it's creator.
+print "Credits to RandomUser.Me for the endless name list!"  # http://randomuser.me (Lets hope that they stay up!)
 
-Gender = str.lower(raw_input("Male, Female, or Random? ")) # Question
-Amount = raw_input("Number of names to generate? ") # Question
-Number = int(Amount) # Couldn't do int(raw_input) here, as it would break for some reason. Redundant, I know.
+Gender = str.lower(raw_input("Male, Female, or Random? "))  # Question
+Amount = raw_input("Number of names to generate? ")  # Question
+Number = int(Amount)  # Couldn't do int(raw_input) here, as it would break for some reason. Redundant, I know.
 
-def Generate(Gender, Number):
+def generate(Gender, Number):
 
-	if Gender == "random": # Random
-		while Number > 0: # While there are still names left to generate, work!
-			req = requests.get("http://api.randomuser.me") # Fetch JSon output from an API.
-			rjson = req.json() # Let the program know that we're working  with JSon.
-			First = rjson['results'][0]['user']['name']['first'] # Fetch the first name.
-			Last = rjson['results'][0]['user']['name']['last'] # Fetch the last name.
-			print First.capitalize() + " " + Last.capitalize() # Lets capitalize the first and last name, because the JSon doesn't do this defaultly.
-			Number -= 1 # Generate one less name (Minus one)
+	if Gender == "random":  # Random
+		while Number > 0:  # While there are still names left to generate, work!
+			req = requests.get("http://api.randomuser.me")  # Fetch JSon output from an API.
+			rjson = req.json()  # Let the program know that we're working  with JSon.
+			First = rjson['results'][0]['user']['name']['first']  # Fetch the first name.
+			Last = rjson['results'][0]['user']['name']['last']  # Fetch the last name.
+			print First.capitalize() + " " + Last.capitalize()  # Lets capitalize the first and last name, because the JSon doesn't do this defaultly.
+			Number -= 1  # Generate one less name (Minus one)
 
-	else: # OR for Male and Female
+	else:  # OR for Male and Female
 		while Number > 0:
 			req = requests.get("http://api.randomuser.me/?gender=" + Gender)
 			rjson = req.json()
@@ -27,4 +28,4 @@ def Generate(Gender, Number):
 			print First.capitalize() + " " + Last.capitalize()
 			Number -= 1
 
-Generate(Gender, Number) # Call the Generate function.
+generate(Gender, Number)  # Call the Generate function.
--- 01 - Heads or Tails.py
+++ 01 - Heads or Tails.py
@@ -1,16 +1,16 @@
-from random import randint # Necessary packages
+from random import randint  # Necessary packages
 
-print "Head or Tails - Bonfire" # Name of the program, and it's creator.
+print "Head or Tails - Bonfire"  # Name of the program, and it's creator.
 Flips = raw_input("Number of coins to flip? ") # Question
-NumFlips = int(Flips) # Convert input to integer.
+NumFlips = int(Flips)  # Convert input to integer.
 
-while NumFlips > 0:	# While there are still coins to flip, work!
-	Number = randint(0,100) # Generate a random integer, 0 to 100.
+while NumFlips > 0:	 # While there are still coins to flip, work!
+	Number = randint(0,100)  # Generate a random integer, 0 to 100.
 
-	if Number >= 50: # If the number is greater or equal to 50, then...
-		Outcome = "Heads" # The outcome is heads.
-	elif Number <= 49: # If the number is less than or equal to 49, then...
-		Outcome = "Tails" # The outcome is tails.
+	if Number >= 50:  # If the number is greater or equal to 50, then...
+		Outcome = "Heads"  # The outcome is heads.
+	elif Number <= 49:  # If the number is less than or equal to 49, then...
+		Outcome = "Tails"  # The outcome is tails.
 
-	print Outcome + " with a number of " + str(Number) # Print the outcome, and it's cooresponding number.
-	NumFlips -= 1 # Reduce the number of needed flips by one.
+	print Outcome + " with a number of " + str(Number)  # Print the outcome, and it's cooresponding number.
+	NumFlips -= 1  # Reduce the number of needed flips by one.
