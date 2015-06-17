96a513597c2a4f3ac90dd2c8196a9b390fcad2cd
Bonfire <bonfire.hf@gmail.com>
2015-06-17 02:49:36 -0400

    Challenge 03 Completed

---
+++ 03 - Years to Seconds.py
@@ -0,0 +1,8 @@
+
+print "Age to Seconds - Bonfire" # Name of the program, and it's creator.
+Age = raw_input("Current age in years: ") # Ask for the age.
+Years = int(Age) # Turn the age into an integer.
+
+Seconds = (Years * 365.2425 * 24 * 60 * 60) # Calculate the seconds using 1 Gregorian year.
+
+print str(Age) + " years is equal to " + str(Seconds) + " seconds." # Print our age in years, to seconds.
